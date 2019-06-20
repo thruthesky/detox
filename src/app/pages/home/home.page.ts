1f92f4a5d538abaf60bd774a34b2f995a1c23ead
jordanmel <jmcatabona0101@gmail.com>
2019-06-20 17:45:48 +0800

    update on slider

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -6,10 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  slideOpts = {
-    initialSlide: 1,
-    speed: 400
-  };
+ 
   constructor() {}
 
 }
