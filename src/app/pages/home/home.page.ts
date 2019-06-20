73738d96dcc45b6fece15c9399156e70ed69d6e1
jordanmel <jmcatabona0101@gmail.com>
2019-06-20 23:34:48 +0800

    updated slider2

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -6,7 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
- 
+  img="https://static1.squarespace.com/static/571fb0c760b5e9a0a98cc71c/5c3cf3b9758d4640ed8e42f1/5c3cf47a1ae6cfa74d78926d/1549515177784/Santa-Barbara-Hotel-Photography-Mark-Skovorodko0003.jpg";
+  status: boolean;
+
+  
   constructor() {}
+
+  imgload() {
+    console.log('asda');
+  }
+
 
 }
