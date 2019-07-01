1f59d4faec3ceadae3874e799a9845b373275df6
jordanmel <jmcatabona0101@gmail.com>
2019-07-01 16:53:25 +0800

    up bottom buttons mobile

--- src/app/components/mobile-home-bottom-buttons/mobile-home-bottom-buttons.module.ts
+++ src/app/components/mobile-home-bottom-buttons/mobile-home-bottom-buttons.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MobileHomeBottomButtonsComponent } from './mobile-home-bottom-buttons.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [ MobileHomeBottomButtonsComponent ],
   exports: [ MobileHomeBottomButtonsComponent ],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
   ]
 })
 export class MobileHomeBottomButtonsModule { }
