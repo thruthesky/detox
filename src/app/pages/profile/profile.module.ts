aebb1ca93829b21d10edc739fa3d19325460a607
JAEHO SONG <thruthesky@gmail.com>
2019-07-08 15:29:29 +0800

    update profile

---
+++ src/app/pages/profile/profile.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ProfilePage } from './profile.page';
+import { HeaderModule } from 'src/app/components/header/header.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProfilePage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes),
+    HeaderModule
+  ],
+  declarations: [ProfilePage]
+})
+export class ProfilePageModule {}
