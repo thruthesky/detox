24117f370ab000999037dadd471cf8e72257d446
JAEHO SONG <thruthesky@gmail.com>
2019-07-10 00:02:06 +0800

    user profile

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -62,8 +62,8 @@ export class ProfilePage implements OnInit {
       gender: ['', [Validators.required]],
       address: ['', [Validators.required]],
       birthday: ['', [Validators.required]],
-      height: ['', [Validators.required, Validators.pattern('[0-9]*'), Validators.min(100), Validators.max(300)]],
-      weight: ['', [Validators.required, Validators.pattern('[0-9]*'), Validators.min(30), Validators.max(200)]]
+      height: ['', [Validators.required, Validators.pattern('[0-9]*'), Validators.min(100), Validators.max(220)]],
+      weight: ['', [Validators.required, Validators.pattern('[0-9]*'), Validators.min(30), Validators.max(160)]]
     });
 
     this.formKeys = Object.keys(this.form.value);
@@ -81,29 +81,29 @@ export class ProfilePage implements OnInit {
       },
       mobile: {
         required: a.t({ en: 'Mobile No. is required.', ko: '핸드폰 번호는 필수 입력 항목입니다.' }),
-        pattern: a.t({ en: 'Please enter a valid number', ko: '핸드폰 번호는 필수 입력 항목입니다.' }),
+        pattern: a.t({ en: 'Please enter a valid number', ko: '핸드폰 번호는 숫자와 +, - 기호만 사용 가능합니다.' }),
       },
       gender: {
-        required: a.t({ en: 'Gender is required.', ko: '핸드폰 번호는 필수 입력 항목입니다.' }),
+        required: a.t({ en: 'Gender is required.', ko: '성별은 필수 선택항목입니다.' }),
       },
       address: {
-        required: a.t({ en: 'Address is required.', ko: '핸드폰 번호는 필수 입력 항목입니다.' }),
+        required: a.t({ en: 'Address is required.', ko: '주소는 필수 입력 항목입니다.' }),
       },
       birthday: {
-        required: a.t({ en: 'Birthday is required.', ko: '핸드폰 번호는 필수 입력 항목입니다.' }),
+        required: a.t({ en: 'Birthday is required.', ko: '생년월일은 필수 입력 항목입니다.' }),
       },
       height: {
-        required: a.t({ en: 'Height is required.', ko: '핸드폰 번호는 필수 입력 항목입니다.' }),
-        pattern: a.t({ en: 'Please input number only.', ko: '핸드폰 번호는 필수 입력 항목입니다.' }),
-        min: a.t({ en: 'Height is too low.', ko: '핸드폰 번호는 필수 입력 항목입니다.' }),
-        max: a.t({ en: 'Height is too high.', ko: '핸드폰 번호는 필수 입력 항목입니다.' }),
+        required: a.t({ en: 'Height is required.', ko: '키는 필수 입력 항목입니다.' }),
+        pattern: a.t({ en: 'Please input number only.', ko: '숫자만 입력해주세요.' }),
+        min: a.t({ en: 'Height is too low.', ko: '키가 너무 작습니다.' }),
+        max: a.t({ en: 'Height is too high.', ko: '키가 너무 큽니다.' }),
       },
 
       weight: {
-        required: a.t({ en: 'Weight is required.', ko: '핸드폰 번호는 필수 입력 항목입니다.' }),
-        pattern: a.t({ en: 'Please input number only.', ko: '핸드폰 번호는 필수 입력 항목입니다.' }),
-        min: a.t({ en: 'Weight is too low.', ko: '핸드폰 번호는 필수 입력 항목입니다.' }),
-        max: a.t({ en: 'Weight is too high.', ko: '핸드폰 번호는 필수 입력 항목입니다.' }),
+        required: a.t({ en: 'Weight is required.', ko: '몸무게는 필수 입력 항목입니다.' }),
+        pattern: a.t({ en: 'Please input number only.', ko: '숫자만 입력해 주세요.' }),
+        min: a.t({ en: 'Weight is too low.', ko: '몸무게가 너무 낮습니다.' }),
+        max: a.t({ en: 'Weight is too high.', ko: '몸무게가 너무 높습니다.' }),
       }
 
     };
@@ -145,7 +145,7 @@ export class ProfilePage implements OnInit {
       this.validate(false);
       return;
     }
-    const data: User = {
+    const reqData: User = {
       address: this.form.value.address,
       display_name: this.form.value.display_name,
       mobile: this.form.value.mobile,
@@ -156,7 +156,11 @@ export class ProfilePage implements OnInit {
     };
 
 
-    this.a.wp.updateProfile(data).subscribe(async (res) => {
+    console.log('reqData', reqData);
+
+    this.a.wp.profileUpdate(reqData).subscribe(async (res) => {
+
+      console.log('user update: ', res);
       (await this.alert.create({
         header: this.a.t({ en: 'update Success', ko: '업데이트 성공.' }),
         // message: this.a.t({ en: 'Please upload your picture.', ko: '회원 정보를 업데이트 해 주시거나 프로그램에 참여를 해 보세요 ^^;' }),
@@ -164,10 +168,11 @@ export class ProfilePage implements OnInit {
       })).present();
 
       this.a.openHome();
-    })
+    });
 
     console.log('onSubmit()');
 
   }
 
 }
+
