064f2b8a3f028c6c0b6ab1ed11975315f074d2c3
jordanmel <jmcatabona0101@gmail.com>
2019-07-19 17:36:16 +0800

    update on form

--- src/app/components/post-edit/post-edit.component.ts
+++ src/app/components/post-edit/post-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from 'src/app/services/app.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-post-edit',
@@ -8,8 +9,30 @@ import { AppService } from 'src/app/services/app.service';
 })
 export class PostEditComponent implements OnInit {
 
-  constructor( public a: AppService) { }
+  form: FormGroup;
+
+
+  constructor( 
+    public a: AppService,
+    public fb: FormBuilder,
+
+    ) {
+
+      this.form = fb.group({
+        title: [''],
+        content: ['',Validators.required],
+        image: [''],
+      });
+
+   }
 
   ngOnInit() {}
+
+
+  onSubmit() {
+    
+  }
+
+
 
 }
--- src/app/components/post-edit/post-edit.module.ts
+++ src/app/components/post-edit/post-edit.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostEditComponent } from './post-edit.component';
 import { IonicModule } from '@ionic/angular';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [PostEditComponent],
   exports: [PostEditComponent],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    ReactiveFormsModule,
   ]
 })
 export class PostEditModule { }
