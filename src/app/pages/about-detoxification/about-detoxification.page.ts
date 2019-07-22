774cf98974646f766c0e5ce5b95df1117a502aa6
jordanmel <jmcatabona0101@gmail.com>
2019-07-22 14:35:41 +0800

    up

--- src/app/pages/about-detoxification/about-detoxification.page.ts
+++ src/app/pages/about-detoxification/about-detoxification.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from 'src/app/services/app.service';
 
 @Component({
   selector: 'app-about-detoxification',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutDetoxificationPage implements OnInit {
 
-  constructor() { }
+  constructor(public a: AppService) { }
 
   ngOnInit() {
   }
