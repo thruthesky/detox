fbd54f55173f86e168ac6c499f0364cae6aa470f
JAEHO SONG <thruthesky@gmail.com>
2019-07-22 23:55:39 +0800

    forum post crud. file crud.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,6 +42,10 @@ export class AppModule {
     private a: AppService
   ) {
     wp.addText({
+      ok: a.t({ en: 'OK', ko: '확인' }),
+      error: a.t({ en: 'ERROR #code', ko: '에러 #code' }),
+      chooseOption: this.a.t({ en: 'Choose an Option', ko: '옵션을 선택하세요' }),
+      notYourPost: this.a.t({ en: 'This is not your point.', ko: '회원님의 글이 아닙니다.' }),
       title: a.t({ en: 'Title', ko: '제목' }),
       content: a.t({ en: 'Content', ko: '내용' }),
       inputTitle: a.t({ en: 'Please input title', ko: '제목을 입력하세요.' }),
@@ -49,7 +53,9 @@ export class AppModule {
       fileUpload: a.t({ en: 'Upload Image', ko: '사진 업로드' }),
       submitPost: a.t({ en: 'Create Post', ko: '글 등록' }),
       titleCreatePost: a.t({ en: 'Writing new post on #name forum', ko: '#name 게시판에 글쓰기' }),
-      discussion: a.t({ en: 'Discussion', ko: '자유토론' })
+      discussion: a.t({ en: 'Discussion', ko: '자유토론' }),
+      postNotFoundToUpdateOnPostList: a.t({ en: 'Cannot find the post to update on the post list.', ko: '게시글 목록에서 업데이트 할 글을 찾을 수가 없습니다.' }),
+      postNotFoundToDeleteOnPostList: a.t({ en: 'Cannot find the post to delete on the post list.', ko: '게시글 목록에서 삭제 할 글을 찾을 수가 없습니다.' }),
     });
   }
 }
