691e832290128d04266d586043a996fd3aa8d991
jordanmel <jmcatabona0101@gmail.com>
2019-07-25 23:05:12 +0800

    up measure detox

--- src/app/pages/measure-detox/measure-detox.page.ts
+++ src/app/pages/measure-detox/measure-detox.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from 'src/app/services/app.service';
 
 @Component({
   selector: 'app-measure-detox',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MeasureDetoxPage implements OnInit {
 
-  constructor() { }
+  constructor(public a: AppService) { }
 
   ngOnInit() {
   }
