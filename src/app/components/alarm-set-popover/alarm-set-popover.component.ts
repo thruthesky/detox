b02a4991160f22368da9278cf7005ce6b2d8a82c
jordanmel <jmcatabona0101@gmail.com>
2019-07-31 20:41:25 +0800

    update on alarm set

--- src/app/components/alarm-set-popover/alarm-set-popover.component.ts
+++ src/app/components/alarm-set-popover/alarm-set-popover.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alarm-set-popover',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlarmSetPopoverComponent implements OnInit {
 
-  constructor() { }
+  constructor(public pop: PopoverController) { }
 
   ngOnInit() {}
 
