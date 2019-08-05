785715ef696985e120f5a66652ab041ac3d9824b
JAEHO SONG <thruthesky@gmail.com>
2019-08-05 18:30:21 +0800

    toxin test

--- src/app/components/toxin-test/toxin-test.component.ts
+++ src/app/components/toxin-test/toxin-test.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from 'src/app/services/app.service';
+import { Post } from 'modules/wordpress-api/services/wordpress-api.interface';
 
 @Component({
   selector: 'app-toxin-test',
@@ -8,8 +9,53 @@ import { AppService } from 'src/app/services/app.service';
 })
 export class ToxinTestComponent implements OnInit {
 
-  constructor(public a: AppService) { }
 
-  ngOnInit() {}
+  guid = 'toxin-test';
+  queries = [];
+  checkes = [];
 
+  showResult = false;
+  constructor(public a: AppService) {
+    a.wp.postGet({ guid: this.guid, autop: false, cacheId: this.guid }).subscribe((post: Post) => {
+      if (!post) {
+        return;
+      }
+      this.resetQueries(post);
+    }, e => a.error(e));
+  }
+
+  ngOnInit() { }
+
+
+  resetQueries(post: Post): void {
+    // console.log('post content: ', post.post_content);
+    this.queries = post.post_content.split('\n');
+    this.checkes = Array(this.queries.length).fill(false);
+  }
+
+  onSubmit() {
+    console.log('onSubmit ', this.checkes);
+    this.showResult = true;
+  }
+
+
+  get scoreWidth(): number {
+    // this.checkes =  [
+    //   true,
+    //   true,
+    //   true,
+    //   true,
+    //   true,
+      
+    //   true,
+    //   true,
+    //   true,
+    //   true,
+      
+      
+    // ]; // TEST
+    console.log(this.checkes.filter(v => v));
+    return this.checkes.filter(v => v).length * 10;
+  }
 }
+
