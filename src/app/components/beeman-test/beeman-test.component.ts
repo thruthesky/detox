338995edd209188d3d9bcd3199698a025f3c7fe6
jordanmel <jmcatabona0101@gmail.com>
2019-08-06 16:33:52 +0800

    up

--- src/app/components/beeman-test/beeman-test.component.ts
+++ src/app/components/beeman-test/beeman-test.component.ts
@@ -14,11 +14,14 @@ export class BeemanTestComponent implements OnInit {
 
   form: FormGroup;
   submit = false;
+  
+  bmi: number;
+
 
   constructor(
     public fb: FormBuilder,
     public a: AppService,
-    ) {
+  ) {
     this.form = fb.group({
       gender: ['', [Validators.required]],
       birthday: ['', [Validators.required]],
@@ -27,7 +30,7 @@ export class BeemanTestComponent implements OnInit {
     });
 
 
-   
+
   }
 
   ngOnInit() { }
@@ -41,37 +44,57 @@ export class BeemanTestComponent implements OnInit {
     }
 
     this.showResult = true;
+    
+
+    const data = {
+      gender: this.form.value.gender,
+      birthday: this.form.value.birthday,
+      height: this.form.value.height,
+      weight: this.form.value.weight,
+    };
+    console.log('data',data);
+
+
+    this.bmi = this.getScore(data);
+
   }
 
   errors(formName: string): any {
     return this.form.get(formName).errors;
   }
 
-  getColor( score: number)  : string {
-    
-    if(score) {
-      if( score <= 14 ) {
+  getColor(score: number): string {
+
+    if (score) {
+      if (score <= 14) {
         return 'green';
       }
-      if( score > 14 && score <= 19 ) {
+      if (score > 14 && score <= 19) {
         return 'blue';
       }
-      if( score > 19  && score <= 28) {
+      if (score > 19 && score <= 28) {
         return 'yellow';
       }
-      if( score > 28 && score <= 33 ) {
+      if (score > 28 && score <= 33) {
         return 'orange';
       }
-      if( score > 33 ) {
+      if (score > 33) {
         return 'red';
       }
-
-
 
     }
 
   }
 
 
+  getScore(data): number {
+    
+  return (data.weight/data.height/data.height) * 10000;
+
+  }
+
+
+
 
 }
+
