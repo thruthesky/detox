2cac346c438c57974909174dad37741e276182c0
JAEHO SONG <thruthesky@gmail.com>
2019-08-06 22:01:14 +0800

    update beeman test

--- src/app/components/beeman-test/beeman-test.component.ts
+++ src/app/components/beeman-test/beeman-test.component.ts
@@ -14,7 +14,7 @@ export class BeemanTestComponent implements OnInit {
 
   form: FormGroup;
   submit = false;
-  
+
   bmi: number;
 
 
@@ -25,8 +25,8 @@ export class BeemanTestComponent implements OnInit {
     this.form = fb.group({
       gender: ['', [Validators.required]],
       birthday: ['', [Validators.required]],
-      height: ['', [Validators.required, Validators.pattern('[0-9]+'), Validators.min(100), Validators.max(220)]],
-      weight: ['', [Validators.required, Validators.pattern('[0-9]+'), Validators.min(30), Validators.max(160)]]
+      height: ['', [Validators.required, Validators.pattern('[0-9\.]+'), Validators.min(100), Validators.max(220)]],
+      weight: ['', [Validators.required, Validators.pattern('[0-9\.]+'), Validators.min(30), Validators.max(160)]]
     });
 
     // this.showResult = true;
@@ -45,7 +45,7 @@ export class BeemanTestComponent implements OnInit {
     }
 
     this.showResult = true;
-    
+
 
     const data = {
       gender: this.form.value.gender,
@@ -53,7 +53,7 @@ export class BeemanTestComponent implements OnInit {
       height: this.form.value.height,
       weight: this.form.value.weight,
     };
-    console.log('data',data);
+    console.log('data', data);
 
 
     this.bmi = this.getScore(data);
@@ -87,11 +87,32 @@ export class BeemanTestComponent implements OnInit {
 
   }
 
+  getText(): string {
 
-  getScore(data): number {
-    
-  return (data.weight/data.height/data.height) * 10000;
+    if (this.bmi) {
+      if (this.bmi <= 18.5) {
+        return '저체중';
+      } else if (this.bmi <= 23) {
+        return '정상';
+      } else if (this.bmi <= 25) {
+        return '과체중';
+      } else if (this.bmi <= 30) {
+        return '비만';
+      } else {
+        return '고도비만';
+      }
 
+    }
+
+  }
+
+
+  getScore(data: any): number {
+
+    const h = data.height / 100;
+    const w = data.weight;
+
+    return Math.round(w / (h * h) * 100) / 100;
   }
 
 
