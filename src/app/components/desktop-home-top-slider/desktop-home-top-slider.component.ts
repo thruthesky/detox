322abd02167ec7e9f6de5227d96920dbac0f1caf
jordanmel <jmcatabona0101@gmail.com>
2019-08-08 17:09:45 +0800

    fixed bug at slide arrow

--- src/app/components/desktop-home-top-slider/desktop-home-top-slider.component.ts
+++ src/app/components/desktop-home-top-slider/desktop-home-top-slider.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, ViewChild, AfterViewInit, ElementRef, AfterViewChecked } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { IonSlides } from '@ionic/angular';
 import { AppService } from 'src/app/services/app.service';
 import { PostSearchOptions, Posts } from 'modules/wordpress-api/services/wordpress-api.interface';
+
+
 
 @Component({
   selector: 'app-desktop-home-top-slider',
@@ -9,7 +11,9 @@ import { PostSearchOptions, Posts } from 'modules/wordpress-api/services/wordpre
   styleUrls: ['./desktop-home-top-slider.component.scss'],
 })
 export class DesktopHomeTopSliderComponent implements OnInit {
-  @ViewChild('slides', { static: true }) slides: IonSlides;
+  @ViewChild('slides', { static: false }) slides: IonSlides;
+
+
 
 
 
@@ -31,7 +35,7 @@ export class DesktopHomeTopSliderComponent implements OnInit {
     this.a.wp.postSearch(req).subscribe(res => {
       console.log('res; ', res);
       this.posts = res;
-    
+
     });
   }
 
@@ -49,5 +53,6 @@ export class DesktopHomeTopSliderComponent implements OnInit {
   prev() {
     this.slides.slidePrev();
   }
+
 
 }
