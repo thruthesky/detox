1d4d324f805eb05a767030390698ef2433401eaf
jordanmel <jmcatabona0101@gmail.com>
2019-08-09 14:06:47 +0800

    testiomial mobile

--- src/app/components/mobile-home-testimonial/mobile-home-testimonial.component.ts
+++ src/app/components/mobile-home-testimonial/mobile-home-testimonial.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from 'src/app/services/app.service';
+import { Posts } from 'modules/wordpress-api/services/wordpress-api.interface';
 
 @Component({
   selector: 'app-mobile-home-testimonial',
@@ -8,10 +9,17 @@ import { AppService } from 'src/app/services/app.service';
 })
 export class MobileHomeTestimonialComponent implements OnInit {
 
+ 
+  posts: Posts = [];
   constructor(
     public a: AppService
-  ) { }
+  ) {
+    a.wp.postSearch({ category_name: 'testimonial', posts_per_page: 4, photo: true }).subscribe(res => {
+      this.posts = res;
+      // console.log('res: ', res);
+    }, e => this.a.error(e));
+  }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
