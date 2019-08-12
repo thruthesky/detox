32ca4b5409d84ef1d05b2890e0b283becf520ac6
JAEHO SONG <thruthesky@gmail.com>
2019-08-12 23:42:00 +0800

    translation

--- src/app/services/component.service.ts
+++ src/app/services/component.service.ts
@@ -16,7 +16,7 @@ export class ComponentService {
     async toast(options: ToastOptions) {
         options.showCloseButton = true;
         if (!options.closeButtonText) {
-            options.closeButtonText = 'Close';
+            options.closeButtonText = '닫기';
         }
         const toast = await this.toastController.create(options);
         toast.present();
