f82f77b773ac0bc2b026f1897c9afc4d59a99bf6
jordanMel <jmcatabona0101@gmail.com>
2019-09-30 14:47:06 +0800

    7detox about ui

--- src/app/pages/about-detoxification/about-detoxification.page.ts
+++ src/app/pages/about-detoxification/about-detoxification.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from 'src/app/services/app.service';
 import { Post } from 'modules/wordpress-api/services/wordpress-api.interface';
+import { text } from 'src/app/services/text';
 
 @Component({
   selector: 'app-about-detoxification',
@@ -10,19 +11,46 @@ import { Post } from 'modules/wordpress-api/services/wordpress-api.interface';
 export class AboutDetoxificationPage implements OnInit {
 
 
-  aboutDetox = {}  as Post;
+  // aboutDetox = {}  as Post;
+
+
+  content = this.a.t(text.inLoading);
+  guid = 'about-detox';
+  mobileSrc = '';
+  desktopSrc = '';
 
   constructor(public a: AppService) {
+    this.a.wp.postGet({ guid: this.guid }).subscribe(post => {
+      console.log('post: post', post);
+      this.pre(post);
 
-    this.a.wp.postGetIn({ guid: a.pageCode.aboutDetox }, this.aboutDetox);
-
-   }
+    });
+  }
 
   ngOnInit() {
   }
 
-  src(post: Post) {
-    return this.a.wp.getPostFileUrl(post);
+  // src(post: Post) {
+  //     return this.a.wp.getPostFileUrl(post);
+  //   }
+
+
+  pre(post: Post) {
+    if (!post || !post.files || !post.files.length) {
+      return;
+    }
+    for (const file of post.files) {
+      if (file.name.indexOf('mobile') >= 0) {
+        this.mobileSrc = file.url;
+      } else if (file.name.indexOf('desktop') >= 0) {
+        this.desktopSrc = file.url;
+      }
+    }
+  }
+
+
+  onEdit(post: Post) {
+    this.pre(post);
   }
 
 }
