61168cc57cb01028584ea7bf337e950a289dc684
JAEHO SONG <thruthesky@gmail.com>
2019-09-30 13:35:28 +0800

    update

--- src/app/services/text.ts
+++ src/app/services/text.ts
@@ -2,5 +2,22 @@ export const text = {
     inLoading: {
         en: 'Loading ... ',
         ko: '로딩중입니다.'
+    },
+    good: {
+        en: 'Good',
+        ko: '좋음'
+    },
+    morning: {
+        en: 'Morning',
+        ko: '아침'
+    },
+    noon: {
+        en: 'Noon',
+        ko: '점심'
+    },
+    evening: {
+        en: 'Evening',
+        ko: '저녁'
     }
 };
+
